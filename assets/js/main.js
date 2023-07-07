a05b9492605889cb60ab6376a9fe37b66f64bf24
Dawson Claybrook <joystickclay@gmail.com>
2023-07-06 21:26:35 -0600

    added starting javascript

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// button section. When button is pressed, set up word and main game
+
+
+
+
+// word section. create event listener to listen for keypress. Set array for the word. Set up array for the user word guess. When a variable in the word guess array is correct, reveal the letter on the screen.
+
+
+
+//timer section. Create a timer, and when the timer ends, if the word has not been guessed, the game is lost, or otherwise, won. 
